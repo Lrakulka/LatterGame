6a147c224cc08021f3a807c8a475c1462bb5ca81
Lrakulka <olecsandr32@gmail.com>
2015-11-27 20:23:05 +0200

    ProgramGif test

--- app/src/main/java/freelance/lattergame/StartPage.java
+++ app/src/main/java/freelance/lattergame/StartPage.java
@@ -1,34 +1,93 @@
 package freelance.lattergame;
 
-import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.AbsoluteLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-import java.util.ArrayList;
+import java.util.Date;
 
-import freelance.lattergame.pan.Pancake;
-import freelance.lattergame.pan.PancakeMoveListener;
-import freelance.lattergame.pan.PancakePan;
-
-public class StartPage extends Activity {
+public class StartPage extends AppCompatActivity {
+    private SurfaceView surfaceView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_page);
-        AbsoluteLayout layout = (AbsoluteLayout) findViewById(R.id.absolute);
-        ArrayList<Pancake> pancakes = new ArrayList<>();
-        pancakes.add((Pancake) findViewById(R.id.Pancake));
-        pancakes.add((Pancake) findViewById(R.id.Pancake5));
-        pancakes.add((Pancake) findViewById(R.id.Pancake2));
-        pancakes.add((Pancake) findViewById(R.id.Pancake3));
-        pancakes.add((Pancake) findViewById(R.id.Pancake4));
-        PancakePan pan = new PancakePan(layout, null);
-        pan.setPancakes(pancakes);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceView.setZOrderOnTop(true);    // necessary
+        surfaceView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Rect[][] position = new Rect[4][3];
+        int[][] time = new int[4][3];
+        int[] rows = new int[4];
+        for (int i = 0; i < position.length; ++i) {
+
+            rows[i] = 3;
+            for (int j = 0; j < position[i].length; ++j) {
+                position[i][j] = new Rect(20, 20, 400, 400);
+                time[i][j] = 500;
+            }
+        }
+        final ProgrammingGif programmingGif = new ProgrammingGif(R.drawable.major, rows,
+                position, time, 1000, -1);
+        programmingGif.init(getResources());
+        Thread thread = new Thread(new Runnable() {
+            Canvas canvas;
+            @Override
+            public void run() {
+                Date time = new Date();
+
+                long l = time.getTime();
+                while (time.getTime() - l < 1000000) {
+                    time = new Date();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    canvas = surfaceView.getHolder().lockCanvas();
+                    if (canvas != null) {
+                        canvas.drawColor(Color.TRANSPARENT);
+                        programmingGif.getWithelethetion(canvas, time.getTime());
+                        surfaceView.getHolder().unlockCanvasAndPost(canvas);
+                    }
+                }
+            }
+        });
+        thread.start();
+
     }
 }
